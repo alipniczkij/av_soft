19a89c58e3e10db90f33797ad0cb13c29f1ea51e
aleksandr <mac@MacBook-Air-Mas.local>
2019-09-24 14:59:59 +0300

    Add description

--- ftp_copy.py
+++ ftp_copy.py
@@ -4,7 +4,7 @@ import threading
 from time import sleep
 
 
-class CopierError(Exception):
+class CopierError(Exception): # Класс обработки ошибок
     pass
 
 
@@ -15,7 +15,7 @@ class Copier:
         self.data = None
         self.read_json()
         try:
-            self.ftp = ftplib.FTP()
+            self.ftp = ftplib.FTP()     # Установка соединения с FTP сервером
             self.ftp.connect(self.data['host'], int(self.data['port']))
             self.ftp.login(self.data['username'], self.data['password'])
         except Exception as err:
@@ -24,12 +24,12 @@ class Copier:
 
     def read_json(self):
         try:
-            with open(self.file_name, 'r') as f:
+            with open(self.file_name, 'r') as f:  # Для каждого потока считываем свои данные из json
                 self.data = json.load(f)
         except Exception as err:
             raise CopierError(f'error read json. {err}')
 
-    def file_upload(self):
+    def file_upload(self):     # Заходим в папку, которая нужна и отсылаем в нее файл в бинарном виде
         try:
             self.ftp.cwd(self.data['path_to'])
             with open(self.data['path_from'] + self.data['file_to_copy'], 'rb') as f:
@@ -44,8 +44,9 @@ class Copier:
 
 if __name__ == '__main__':
     while True:
-        json_file = input('Enter json file name or exit: ')
+        json_file = input('Enter json file name or exit: ') # Пишем как называется конфигурационный файл
         if json_file == 'exit':
             break
-        thr = threading.Thread(target=Copier, args=(json_file,))
+        thr = threading.Thread(target=Copier, args=(json_file,)) # Можем написать несколько файлов. Для каждого будет
+                                                                # выделен свой поток
         thr.start()
