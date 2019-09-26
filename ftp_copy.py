bf6867aa4044e79f0c0008827a6095bce36cadcb
aleksandr <mac@MacBook-Air-Mas.local>
2019-09-26 14:49:53 +0300

    fix

--- ftp_copy.py
+++ ftp_copy.py
@@ -1,11 +1,7 @@
 import ftplib
 import json
 import threading
-from time import sleep
-
-
-class CopierError(Exception): # Класс обработки ошибок
-    pass
+from queue import Queue
 
 
 class Copier:
@@ -13,40 +9,64 @@ class Copier:
     def __init__(self, json_name):
         self.file_name = json_name
         self.data = None
+        self.q = Queue()
         self.read_json()
+
+    def log(self):
         try:
-            self.ftp = ftplib.FTP()     # Установка соединения с FTP сервером
-            self.ftp.connect(self.data['host'], int(self.data['port']))
-            self.ftp.login(self.data['username'], self.data['password'])
-        except Exception as err:
-            raise CopierError(f'error create connection. {err}')
-        self.file_upload()
+            ftp = ftplib.FTP()  # Установка соединения с FTP сервером
+            ftp.connect(self.data['host'], int(self.data['port']))
+            ftp.login(self.data['username'], self.data['password'])
+        except ftplib.error_perm:
+            print('FTP error')
+        else:
+            return ftp
 
     def read_json(self):
         try:
             with open(self.file_name, 'r') as f:  # Для каждого потока считываем свои данные из json
                 self.data = json.load(f)
-        except Exception as err:
-            raise CopierError(f'error read json. {err}')
+                for file in self.data['file_to_copy']:  # Заполняем очередь файлами, которые необходимо скопировать
+                    self.q.put(file)
+        except IsADirectoryError:
+            print('It is a directory')
+        except FileNotFoundError:
+            print('File not found')
+        except OSError as err:
+            print(f'System error:\n{err}')
 
-    def file_upload(self):     # Заходим в папку, которая нужна и отсылаем в нее файл в бинарном виде
-        try:
-            self.ftp.cwd(self.data['path_to'])
-            with open(self.data['path_from'] + self.data['file_to_copy'], 'rb') as f:
-                self.ftp.storbinary('STOR ' + self.data['file_to_copy'], f)
-        except Exception as err:
-            raise CopierError(f'error with uploading. {err}')
-        else:
-            sleep(10)
-            print('File was uploaded', self.data['file_to_copy'])
-            self.ftp.close()
+    def threading_files(self):
+        ftp = self.log()
+        for _ in range(len(self.data['file_to_copy'])):
+            threading.Thread(target=self.file_upload(ftp)).start()  # Запускаем столько потоков, сколько файлов нам
+            # нужно скопировать
+
+    def file_upload(self, ftp):  # Заходим в папку, которая нужна и отсылаем в нее файл в бинарном виде
+        while True:
+            if self.q.empty():  # Если очередь пустая заканчиваем работу
+                break
+            file = self.q.get()  # Достаем название файла из очереди
+            try:
+                ftp.cwd(self.data['path_to'])
+            except ftplib.all_errors:  # Заходим в папку на сервере, в которую надо скопировать файл
+                ftp.mkd(self.data['path_to'])  # Если папки такой нет, создаем
+                ftp.cwd(self.data['path_to'])
+            try:
+                with open(self.data['path_from'] + file, 'rb') as f:
+                    ftp.storbinary('STOR ' + file, f)
+            except FileNotFoundError:
+                print('File not found', file)
+            except ftplib.error_perm:
+                print('File was not uploaded', file)
+            else:
+                print('File was uploaded', file)
+                self.q.task_done()
+        ftp.close()
 
 
 if __name__ == '__main__':
-    while True:
-        json_file = input('Enter json file name or exit: ') # Пишем как называется конфигурационный файл
-        if json_file == 'exit':
-            break
-        thr = threading.Thread(target=Copier, args=(json_file,)) # Можем написать несколько файлов. Для каждого будет
-                                                                # выделен свой поток
-        thr.start()
+    json_file = input('Enter json file name or exit: ')  # Пишем как называется конфигурационный файл
+    if json_file == 'exit' or json_file == '':
+        exit()
+    copy = Copier(json_file)
+    copy.threading_files()
